5cab8567eb8d56fe27058e1a5607762b6ae7e767
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-09-23 10:21:06 +0200

    exer 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+const bodyColor = document.body;
+
+let idInterval = null;
+startButton.addEventListener('click', () => {
+  if (idInterval === null)
+    idInterval = setInterval(() => {
+      bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  console.log(idInterval);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+});
+stopButton.addEventListener('click', () => {
+  clearInterval(idInterval);
+  idInterval = null;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
