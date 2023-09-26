90eb86ce143cf50cab1ac9720205446f3a5c7fb9
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-09-26 09:10:19 +0200

    exer 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+'use strict';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const handleSubmit = evt => {
+  evt.preventDefault();
+  const firstDelay = Number.parseInt(evt.target.elements['delay'].value);
+  const delayStep = Number.parseInt(evt.target.elements['step'].value);
+  const amount = Number.parseInt(evt.target.elements['amount'].value);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, firstDelay + delayStep * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+};
+
+form.addEventListener('submit', handleSubmit);
